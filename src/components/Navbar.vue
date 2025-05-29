1723c53847680e90467872f0ff3be5bbc3f2ff63
evangeliaSk <evangelia.skouloudi@alphaomegazed.com>
2025-05-30 00:58:43 +0300

    Create the navbar of the page and the links inside navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="bg-blue-600 text-white p-4">
+    <ul class="flex space-x-4">
+      <navbar-link page="home" :key="1"></navbar-link>
+      <navbar-link page="collections" :key="1"></navbar-link>
+      <navbar-link page="about" :key="1"></navbar-link>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import NavbarLink from './NavbarLink.vue'
+
+export default {
+  components: { NavbarLink },
+  name: 'NavbarMenu',
+}
+</script>
---
+++ src/components/NavbarLink.vue
@@ -0,0 +1,19 @@
+<template>
+  <li>
+    <router-link
+      :to="`/${page}`"
+      class="nav-link"
+      active-class="active"
+      aria-current="page"
+      :title="`This link goes to ${page} the page`"
+    >
+      {{ page }}</router-link
+    >
+  </li>
+</template>
+
+<script lang="ts">
+export default {
+  props: ['page'],
+}
+</script>
