a31b9bff01facf8eb98ed3e986e71957438ffb65
evangeliaSk <evangelia.skouloudi@alphaomegazed.com>
2025-05-31 01:09:25 +0300

    import Carousel to show jokes

--- src/components/JokesCarousel.vue
+++ src/components/JokesCarousel.vue
@@ -1,17 +1,17 @@
-<template>
-  <carousel-3d>
-    <slide :index="0"> Slide 1 Content </slide>
-    <slide :index="1"> Slide 2 Content </slide>
-  </carousel-3d>
-</template>
+<script lang="ts" setup>
+import '@splidejs/vue-splide/css'
+import JokeCard from './JokeCard.vue'
 
-<script lang="ts">
-import { Carousel3d, Slide } from 'vue-carousel-3d'
-
-export default {
-  components: {
-    Carousel3d,
-    Slide,
-  },
-}
+defineProps(['jokes'])
 </script>
+
+<template>
+  <Splide
+    :options="{ rewind: true, perPage: 2, gap: '1rem', arrows: true, pagination: true }"
+    aria-label="Joke Carousel"
+  >
+    <SplideSlide v-for="joke in jokes" :key="joke.id">
+      <JokeCard :joke="joke" />
+    </SplideSlide>
+  </Splide>
+</template>
