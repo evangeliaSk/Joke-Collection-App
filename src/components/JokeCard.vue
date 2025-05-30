cb47de1b518009edcdb4d49167531cfe68937199
evangeliaSk <evangelia.skouloudi@alphaomegazed.com>
2025-05-30 13:45:45 +0300

    Create jokecard as component and import jokesCarousel

---
+++ src/components/JokeCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    :key="joke.id"
+    class="flex flex-col bg-orange-200 text-black p-4 gap-1.5 rounded-xl min-h-fit"
+  >
+    <p class="font-bold">{{ joke.setup }}</p>
+    <p class="font-thin">{{ joke.punchline }}</p>
+
+    <div class="flex gap-1.5">
+      <div>icon</div>
+      <div>icon</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: ['joke'],
+}
+</script>
---
+++ src/components/JokesCarousel.vue
@@ -0,0 +1,17 @@
+<template>
+  <carousel-3d>
+    <slide :index="0"> Slide 1 Content </slide>
+    <slide :index="1"> Slide 2 Content </slide>
+  </carousel-3d>
+</template>
+
+<script lang="ts">
+import { Carousel3d, Slide } from 'vue-carousel-3d'
+
+export default {
+  components: {
+    Carousel3d,
+    Slide,
+  },
+}
+</script>
