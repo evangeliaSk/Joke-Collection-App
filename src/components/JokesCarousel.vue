c74067c0f17870c8c2fda355922cd46e68d88860
evangeliaSk <evangelia.skouloudi@alphaomegazed.com>
2025-06-01 20:32:40 +0300

    carousel  modify to have one per page to mobile view

--- src/components/JokesCarousel.vue
+++ src/components/JokesCarousel.vue
@@ -1,13 +1,25 @@
 <script lang="ts" setup>
 import '@splidejs/vue-splide/css'
 import JokeCard from './JokeCard.vue'
+import type { Joke } from '../jokesClient'
 
-defineProps(['jokes'])
+defineProps<{ jokes: Joke[] }>()
 </script>
 
 <template>
   <Splide
-    :options="{ rewind: true, perPage: 2, gap: '1rem', arrows: true, pagination: true }"
+    :options="{
+      rewind: true,
+      perPage: 2,
+      gap: '1rem',
+      arrows: true,
+      pagination: true,
+      breakpoints: {
+        640: {
+          perPage: 1,
+        },
+      },
+    }"
     aria-label="Joke Carousel"
   >
     <SplideSlide v-for="joke in jokes" :key="joke.id">
